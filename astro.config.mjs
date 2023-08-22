aa6bb82c385104df09f8bf6504877612d22417b6
cchaniotaki <cchaniotaki@ubitech.eu>
2023-08-21 23:51:07 -0700

    add deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
@@ -8,12 +8,13 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://https://cchaniotaki.github.io',
-  integrations: [mdx(), sitemap(), tailwind(), image(
-    {
-      serviceEntryPoint: '@astrojs/image/sharp',
-      cacheDir: "./.cache/image",
-      logLevel: 'debug',
-    }
-  )]
+    site: 'https://https://cchaniotaki.github.io',
+    base: '/cchaniotaki.github.io',
+    integrations: [mdx(), sitemap(), tailwind(), image(
+        {
+            serviceEntryPoint: '@astrojs/image/sharp',
+            cacheDir: "./.cache/image",
+            logLevel: 'debug',
+        }
+    )]
 });
