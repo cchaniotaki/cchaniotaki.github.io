ac6559bbd6fa670638136caff47710321c295061
cchaniotaki <cchaniotaki@ubitech.eu>
2023-08-23 00:47:38 -0700

    fix bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,12 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
     site: 'https://cchaniotaki.github.io',
-    base: '/cchaniotaki.github.io'
+    base: '/cchaniotaki.github.io',
+    integrations: [mdx(), sitemap(), tailwind(), image(
+        {
+            serviceEntryPoint: '@astrojs/image/sharp',
+            cacheDir: "./.cache/image",
+            logLevel: 'debug',
+        }
+    )]
 });
