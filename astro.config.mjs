875f77488f744ffc667c5e8583fa05e3804a132e
Christina Chaniotaki <christinachaniotaki96@gmail.com>
2025-02-19 19:58:36 -0800

    fix deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,11 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 import image from "@astrojs/image";
 
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://cchaniotaki.github.io',
+    base: 'cchaniotaki.github.io',
     integrations: [mdx(), sitemap(), tailwind(), image(
         {
             serviceEntryPoint: '@astrojs/image/sharp',
